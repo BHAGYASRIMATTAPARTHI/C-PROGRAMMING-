fb78dd1932c117fa776aefe616ebb4c058dae2c8
BHAGYASRIMATTAPARTHI <mattaparthibhagyasri5@gmail.com>
2025-10-28 11:54:57 +0530

    Add files via upload

---
+++ area of triangle.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+	float area,b,h;
+	printf("enter b,h vaules ::");
+	scanf("%f%f",&b,&h);
+	area = 0.5 * b * h;
+	printf("area of triangle = %f",area);
+	return 0;
+}
