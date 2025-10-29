6e9c1c38c896029236a3de8353356b8e1d1d5fd5
BHAGYASRIMATTAPARTHI <mattaparthibhagyasri5@gmail.com>
2025-10-29 20:09:29 +0530

    Add files via upload

---
+++ even or odd by if-else.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	int x;
+	printf("enter x vaule::");
+	scanf("%d",&x);
+	if(x%2==0)
+	{
+		printf("x is even");
+	}
+	else
+	{
+		printf("x is odd");
+	}
+	return 0;
+}
