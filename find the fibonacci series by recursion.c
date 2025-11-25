c39ad924f9ea09f8c3c7c179cf37732c6442a007
BHAGYASRIMATTAPARTHI <mattaparthibhagyasri5@gmail.com>
2025-11-25 15:37:37 +0530

    Add files via upload

---
+++ find the fibonacci series by recursion.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int fib(int);
+int main()
+{
+	int n,i;
+	printf("enter size of the series::");
+	scanf("%d",&n);
+	for(i=0;i<=n;i++)
+	{
+		printf(" %d",fib(i));
+	}
+	return 0;
+}
+int fib(int i)
+{
+	if(i<=1)
+	return i;
+	else 
+	return fib(i-2)+fib(i-1);
+}
